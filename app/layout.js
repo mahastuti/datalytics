918954afd91e929d436ae9bd0450ac4a8ca71a5a
taketoputa <orcalinn@.gmail.com>
2025-05-08 02:38:17 +0700

    Initial Commit

---
+++ app/layout.js
@@ -0,0 +1,34 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+
+export const metadata = {
+  title: "QuickCart - GreatStack",
+  description: "E-Commerce with Next.js ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`} >
+          <Toaster />
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
